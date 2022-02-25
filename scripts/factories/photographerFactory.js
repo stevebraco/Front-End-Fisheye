24e0c75b5f9372d995d18653f37ccbcc5c647382
steve <steve.braco@gmail.com>
2022-02-25 15:07:56 +0100

    finish index

--- scripts/factories/photographerFactory.js
+++ scripts/factories/photographerFactory.js
@@ -1,16 +1,35 @@
+import { createElement } from '../utils/utils.js';
 function photographerFactory(data) {
   const { name, portrait } = data;
 
   const picture = `assets/photographers/${portrait}`;
 
   function getUserCardDOM() {
-    const article = document.createElement('article');
-    const img = document.createElement('img');
-    img.setAttribute('src', picture);
-    const h2 = document.createElement('h2');
-    h2.textContent = name;
-    article.appendChild(img);
-    article.appendChild(h2);
+    const {
+      id,
+      name,
+      portrait,
+      city,
+      country,
+      tagline: description,
+      price,
+    } = data;
+
+    const picture = `assets/photographers/${portrait}`;
+    const location = `${city}, ${country}`;
+    const pricePerDay = `${price}€/jour`;
+    const model = `
+        <a aria-label='En savoir plus sur ${name}' class='photographer__link' href='photographer.html?id=${id}'>
+          <img class='photographer__img' src=${picture} alt='portrait ${name}' />
+          <h2 class='photographer__name'>${name}</h2>
+        </a>
+        <span class='photographer__location'>${location}</span>
+        <span class='photographer__description'>${description}</span>
+        <span class='photographer__price'>${pricePerDay}</span>
+        `;
+
+    const article = createElement('article', ['photographer'], model, null);
+
     return article;
   }
   return { name, picture, getUserCardDOM };
